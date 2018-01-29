59b12b6c07cee43652cc73e6e7fd6c07d40a8191
kvasnicka.simon <kvasnicka.simon@gmail.com>
2018-01-29 03:05:39 -0500

    added search mode toggle

---
+++ src/components/SearchModeToggle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-model="value" type="radio" name="search-mode" id="price" value="price">
+    <label for="price">Cheapest</label>
+    <input v-model="value" type="radio" name="search-mode" id="time" value="time">
+    <label for="time">Fastest</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import { SearchModeType } from "../types";
+
+@Component
+export default class SearchModeToggleComponent extends Vue {
+  @Prop() value: SearchModeType;
+
+  @Watch('value')
+  onPropertyChanged(value: SearchModeType) {
+    this.$emit('input', value);
+  }
+}
+</script>
+
+<style>
+</style>
