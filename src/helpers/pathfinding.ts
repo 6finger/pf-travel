531cc354ef7f3b888c4af2e9265c1dc44522747b
kvasnicka.simon <kvasnicka.simon@gmail.com>
2018-01-30 05:56:56 -0500

    added comments, code cleaning

--- src/helpers/pathfinding.ts
+++ src/helpers/pathfinding.ts
@@ -4,33 +4,35 @@ import { getDealPrice, getDealTime } from './format';
 import * as Graph from "node-dijkstra";
 
 export function findTrip(deals: DealType[], from: string, to: string, searchMode: SearchModeType) {
-  console.log('trying to get path >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-  
+  // An oriented graph with cities as vertices.
+  // Edge between two vertices is the best deal between these two cities.
   var graphMap: { [key: string]: { [key: string]: number } } = {};
+  // A graph copying the same structure as the weighted graph
+  // but containing the deals that were used to create the graph.
   var dealsMap: { [key: string]: { [key: string]: DealType } } = {};
   
   deals.forEach((deal: DealType) => {
     graphMap[deal.departure] = graphMap[deal.departure] || {};
     dealsMap[deal.departure] = dealsMap[deal.departure] || {};
 
-    let weight = searchMode === SearchModeType.Price ? getDealPrice(deal) : getDealTime(deal);
-    let currentWeight = graphMap[deal.departure][deal.arrival];
+    // There can be multiple links between cities but we are interested only in the one with smallest weight.
+    // We are looking for a path with the smallest weight. For any two paths that differ
+    // only in one edge, the path using the edge with smaller weight will have smaller weight in total.
+    var weight = searchMode === SearchModeType.Price ? getDealPrice(deal) : getDealTime(deal);
+    var currentWeight = graphMap[deal.departure][deal.arrival];
     if (currentWeight == null || weight < currentWeight) {
       graphMap[deal.departure][deal.arrival] = weight;
       dealsMap[deal.departure][deal.arrival] = deal;
     }
   });
   
-  let g = new Graph(graphMap);
-  let path = g.shortestPath(from, to);
+  var g = new Graph(graphMap);
+  var path: string[] = g.shortestPath(from, to);
   
-  if (!path || !path.length) {
-    return [];
-  }
-  
-  let results: DealType[] = [];
-  path.forEach((city: string, index: number) => {
+  var results: DealType[] = [];
+  path && path.forEach((city: string, index: number) => {
     if (index !== path.length - 1) {
+      // Get the original deal between two cities.
       results.push(dealsMap[city][path[index+1]]);
     }
   });
